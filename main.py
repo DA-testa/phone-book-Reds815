cc45176c68d7d91fa2060fc5d84d9b0d7829d2b9
Reds815 <Reds-Ricards.Polis@edu.rtu.lv>
2023-04-02 22:30:03 +0300

    update

--- main.py
+++ main.py
@@ -8,18 +8,52 @@ class Query:
             self.name = query[2]
 
 def read_queries():
+    #y = input('\n').split()
     n = int(input())
-    return [Query(input().split()) for i in range(n)]
-
+    
+    while not (n > 0 and n <= 1000000):
+      print('\n'"Invalid COUNT input, try again COUNT input:")
+      n = int(input())
+    
+    y = []
+    y2 = []
+    for i in range(n):
+        y = input().split()
+        
+        if (y[0] == 'add'):
+          while not (len(y) == 3):
+            print('\n'"Invalid ADD COMMAND input, try again ADD COMMAND input:")
+            y = input().split()
+        else:
+          while not (len(y) == 2):
+            print('\n'"Invalid FIND or DEL COMMAND input, try again FIND or DEL COMMAND input:")
+            y = input().split()
+          
+        c1=y[1]
+        while not (y[1].isdigit() and (len(y[1]) <= 7) and (len(y[1]) >= 1) and (c1[0:1] != '0')):
+          print('\n'"Invalid NUMBER input, try again NUMBER input:")
+          y[1] = input('\n')
+          c1=y[1]
+          
+        if y[0] == 'add':
+          while not (len(y[2]) <= 15 and y[2].isalpha()):
+            print('\n'"Invalid NAME input, try again NAME input:")
+            y[2] = input('\n')
+  
+        y2.append(Query(y))
+      
+    return y2
+    #return [Query(input().split()) for i in range(n)]
+   
 def write_responses(result):
     print('\n'.join(result))
 
 def process_queries(queries):
-    result = []
+    result = [] # ko drukā uz ekrāna
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    contacts = [] # add pieprasījumi tiek krāti šajā sarakstā
     for cur_query in queries:
-        if cur_query.type == 'add':
+        if cur_query.type == 'add': # cur_query ir tekošais pieprasījums
             # if we already have contact with such number,
             # we should rewrite contact's name
             for contact in contacts:
@@ -28,6 +62,8 @@ def process_queries(queries):
                     break
             else: # otherwise, just add it
                 contacts.append(cur_query)
+                # eksperimentam:
+                #print(cur_query.type)
         elif cur_query.type == 'del':
             for j in range(len(contacts)):
                 if contacts[j].number == cur_query.number:
@@ -44,4 +80,3 @@ def process_queries(queries):
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
